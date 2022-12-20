ca6cf5313cbef56f9b86d71721ee93af2d79738b
Anatoliy Katelnitskiy <katel1998@yandex.ru>
2022-12-20 15:57:57 +0300

    sort fix

--- src/app.js
+++ src/app.js
@@ -1,14 +1,3 @@
 export default function sortByHealth(arr) {
-  const arrHealth = [];
-  const newArr = [];
-  arr.forEach((element) => {
-    arrHealth.push(element.health);
-  });
-  arrHealth.sort((a, b) => b - a);
-
-  for (let i = 0; arrHealth.length > i; i += 1) {
-    const foundElement = arr.find((e) => e.health === arrHealth[i]);
-    newArr.push(foundElement);
-  }
-  return newArr;
+  return arr.sort((a, b) => b.health - a.health);
 }
